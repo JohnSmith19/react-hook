7015b3d9f001ce4a82b5255caa4be86df7006fdc
John Smith <unknown.prop@gmail.com>
2019-04-03 09:31:40 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Counter from "./useState/Counter";
-import Info from "./useState/Info";
+import InfoEffect from "./useEffect/InfoEffect";
 
 class App extends Component {
   render() {
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Info />
+          <InfoEffect />
           <Counter />
         </header>
       </div>
