a362a16408573d864a4b626da53ee6d9c27008f8
John Smith <unknown.prop@gmail.com>
2019-04-03 09:35:26 +0900

    useEffect

--- src/useEffect/InfoEffect.js
+++ src/useEffect/InfoEffect.js
@@ -3,10 +3,17 @@ import React, { useState, useEffect } from "react";
 const InfoEffect = () => {
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
+  // useEffect: react component가 render 될때마다 특정 작업을 수행하도록 설정한다.
+  // cpdm & cpdu
   useEffect(() => {
     console.log("rendering complete!!");
     console.log({ name, nickname });
   });
+
+  // cpdm
+  useEffect(() => {
+    console.log("component mount");
+  }, []);
 
   const onChangeName = e => {
     setName(e.target.value);
